af8816bf08e90828d424ee72ac8cd1b6617c63c2
Michel Bystranowski <michel@vertige.org>
2014-04-04 17:43:48 +0200

    crée un formulaire bidon qui surcharge le formulaire d'ajout de slides liés à un slideshow

---
+++ formulaires/editer_slides_slideshow.php
@@ -0,0 +1,13 @@
+<?php
+
+function formulaires_editer_slides_slideshow_saisies ($id_slideshow) {
+
+    return array(
+        array(
+            'saisie' => 'input',
+            'options' => array(
+                'nom' => 'prout',
+            ),
+        ),
+    );
+}
